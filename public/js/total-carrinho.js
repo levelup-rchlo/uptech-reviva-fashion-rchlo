3038ac290187e4fe626d3a2108adeaf7bbb747b9
alanFMA <alanmonteiro105@gmail.com>
2022-05-12 16:11:34 -0300

    [card01]Cálculo dos Totais

--- public/js/total-carrinho.js
+++ public/js/total-carrinho.js
@@ -2,4 +2,4 @@ const { nodeModuleNameResolver } = require("typescript")
 
 const carrinho = [39.90, 75.99, 24, 12, 109.99, 83.99]
 var total = carrinho.reduce((total, carrinho) => total + carrinho, 0)
-console.log(total)
+console.log(total) 
