ddf72c200ffaec7ba43b8407e424eb3861d01e37
Italla Felyne <italla.ferreira@riachuelo.com.br>
2022-05-13 22:26:15 -0300

    Inserindo Cards completos até o 5

---
+++ public/js/carrinho-matriz.js
@@ -0,0 +1,19 @@
+const carrinho = [
+    ['Calça Jeans Masculina', 3, 109.89],
+    ['Camiseta Básica Feminina', 2, 19.90],
+    ['Boné Unissex', 1, 25.00],
+    ['Saia Feminina', 1, 35.00],
+    ['Calça Jeans Feminina', 1, 150.00]
+  ]
+
+var produtosRepetidos = [];
+
+for(indice = 0; indice < carrinho.length; indice++){
+    if(carrinho[indice] === 'Saia Feminina'){
+        
+    }
+}
+
+console.log(produtosRepetidos)
+
+/*AINDA SEM TERMINAR*/
