c98aa0b2aec8f0fd8e5a92f91a51be8f80590366
alanFMA <alanmonteiro105@gmail.com>
2022-05-17 12:45:36 -0300

    [card 02] Calculo do Total com Taxas - ATUALIZADO

--- public/js/total-carrinho-taxado.js
+++ public/js/total-carrinho-taxado.js
@@ -4,10 +4,10 @@ const minTaxa = 70;
 const taxa = 1.07;
 const carrinho = [39.90, 75.99, 24, 12, 109.99, 83.99];
 
-const total = carrinho.reduce((total, carrinho) => {
-  const taxaSe = carrinho >= minTaxa;
-  const valorTaxa = taxaSe ? carrinho * taxa : carrinho
+const total = carrinho.reduce((total, atual) => {
+  const temTaxa = atual >= minTaxa;
+  const valorTaxa = temTaxa ? atual * taxa : atual
   return total + valorTaxa;
-})
+},0)
 
 console.log(total)
