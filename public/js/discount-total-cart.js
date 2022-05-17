daafcc9bb74b9642dc09f6b38e8743e0967e9c43
brMonteiro-G <brgabriel.monteiro@gmail.com>
2022-05-17 16:33:58 -0300

    refactor(card-3): code review

--- public/js/discount-total-cart.js
+++ public/js/discount-total-cart.js
@@ -1,43 +1,39 @@
 const cart = [25.99, 27.83, 35.80, 35.80, 35.80, 44, 53.90, 25.99] 
 const produtosDuplicados = []
 
-const listaParcialSemDuplicados = 
+const filtroDeDuplicatas = 
 cart.filter((element, index) => {
-    if (cart.indexOf(element) === index) {
-        return element
+    if( cart.indexOf(element) === index){
+        return element 
     }
-    else {
-        if(!produtosDuplicados.includes(element)){
-            produtosDuplicados.push(element)
-        }
+    if(!produtosDuplicados.includes(element)){
+        produtosDuplicados.push(element)
+    }{
     }
-
+    
 })
 
-for (let i = 0; i < 1; i++) {
-    let elementoListaProdutoDuplicado = produtosDuplicados[i];
-    
-    for (let k = 0; k < cart.length; k++) {
-        const elementoDoCart = cart[k];
-
-        if(elementoDoCart === elementoListaProdutoDuplicado){
-            let produtoComDescontoAplicado =  elementoListaProdutoDuplicado*0.9
-            cart.splice(cart.indexOf(elementoDoCart),1,produtoComDescontoAplicado);
-            elementoListaProdutoDuplicado = produtosDuplicados[i+1]        
-        }
+const buscaDuplicatas= cart.map(element => {
+    if(produtosDuplicados.includes(element)){
+    produtosDuplicados.pop(element)
+    let novoElemento =  element*0.9
+    return novoElemento
+    }else{
+        return element
     }
-}
+})
 
-const totalDoCarrinho = cart.reduce((acc, e) => {
+
+const totalDoCarrinho = buscaDuplicatas.reduce((acc, e) => {
     return acc += e
 })
 
+
 console.log("---------------------------------------------------");
-console.log("Carrinho de compras com descontos aplicados" + "[" + cart + "]");
+console.log(`Carrinho de compras com descontos aplicados [ ${buscaDuplicatas} ]`);
 console.log("---------------------------------------------------");
 console.log("Total do carrinho " + totalDoCarrinho.toFixed(2));
 
-produtosDuplicados.push()
 
 
 
