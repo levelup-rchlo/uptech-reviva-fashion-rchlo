2fbb996271cfe7b21f286306761e42f041c69cb8
alanFMA <alanmonteiro105@gmail.com>
2022-05-17 12:41:33 -0300

    [card 03] Calculo do Total com Descontos - Atualizado

--- public/js/carrinho-total-descontos.js
+++ public/js/carrinho-total-descontos.js
@@ -1,27 +1,27 @@
 const carrinho = [25.99, 27.83, 35.80, 35.80, 35.80, 44, 53.90, 25.99];
-const desconto = 0.9;
 
-const calculaTotal = (valor1, valor2) => valor1 + valor2;
+const carrinhoProcessado = [];
 
-const produtosParaDesconto = [];
+const carrinhoNovo = carrinho.map((valorAtual) => {
+  	const incidencia = carrinhoProcessado.filter((elemento) => {
+    	if (elemento === valorAtual) {
+      	return elemento
+    	}
+  	})
+  	console.log({
+  		incidencia
+  	})
+  	carrinhoProcessado.push(valorAtual)
+  	if (incidencia.length === 1) {
+  		valorAtual = valorAtual * .9;
+  	} 
+  	console.log(valorAtual)
+  	return valorAtual;
+	})
+	const total = carrinhoNovo.reduce((acc, cur) => {
+  	return acc + cur;
+  },0)
 
-const totalSemDesconto = carrinho
-    .map((valorProduto, indiceAtual) => {
-        if (!produtosParaDesconto.includes(valorProduto) && carrinho.includes(valorProduto, indiceAtual + 1)) {
-            produtosParaDesconto.push(valorProduto);
-        }
-        return valorProduto;
-        
-    }).reduce(calculaTotal, 0);
-
-const totalDosProdutosComDesconto = produtosParaDesconto
-    .reduce((acc, valorAtual) => calculaTotal(acc, valorAtual * desconto), 0);
-
-const totalASerSubtraindoDoTotal = produtosParaDesconto.reduce(calculaTotal, 0);
-
-console.log(produtosParaDesconto);
-console.log(totalSemDesconto)
-console.log(totalASerSubtraindoDoTotal)
-console.log(totalDosProdutosComDesconto)
-
-console.log(totalSemDesconto - totalASerSubtraindoDoTotal + totalDosProdutosComDesconto)
+console.log({
+	carrinhoProcessado, carrinhoNovo, total
+})
