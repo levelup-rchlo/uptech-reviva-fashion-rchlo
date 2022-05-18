b01711b1250d13989e8c249757644b16e2ffd4cc
Isaque Terra <terraisaque@gmail.com>
2022-05-17 21:54:06 -0300

    card 6 - Recalculando o total

--- public/js/carrinho-total-descontos-taxas.js
+++ public/js/carrinho-total-descontos-taxas.js
@@ -1,59 +1,51 @@
-const produtos = ['Camiseta Básica','Camiseta Polo', 'Bermuda Moletom', 'Calça Jeans Masculina', 'Camiseta Básica', 'Calça Jeans Feminina', 'Camiseta Básica'];
+const produtos = ['Camiseta Básica', 'Camiseta Polo', 'Bermuda Moletom', 'Calça Jeans Masculina', 'Camiseta Básica', 'Calça Jeans Feminina', 'Camiseta Básica'];
 
 const precos = [29.90, 49.90, 35, 89.99, 29.90, 109.99, 29.90]
 const descontoTodasCamisetas = 0.9;
 const taxaCalcaJeans = 3.99;
-
+const descontosAdicional = 0.95;
 // ----------------------------------------------------------------------------------------------------
 // Pegando o índice pelo nome do produto e separando o índex
 
-let pegaIndice = (nomeProduto) => { 
-    let somaIndices = [];
-    produtos.filter((modeloProduto, index) => {
-        if (modeloProduto.includes(nomeProduto)) {
-            somaIndices.push(index);
-        }})
-        return somaIndices;
+const pegaIndice = (arrayItem, nomeProduto) => {
+    const somaIndices = [];
+    arrayItem.filter((modeloProduto, index) => modeloProduto.includes(nomeProduto) ? somaIndices.push(index) : modeloProduto)
+    return somaIndices;
 }
 
-let indiceCamisetas = pegaIndice("Camiseta");
-let indiceCalcaJeans = pegaIndice("Calça Jeans");
-let indiceBermuda = pegaIndice("Bermuda");
+const indiceCamisetas = pegaIndice(produtos, "Camiseta");
+const indiceCalcaJeans = pegaIndice(produtos, "Calça Jeans");
+const indiceBermuda = pegaIndice(produtos, "Bermuda");
 
 // ----------------------------------------------------------------------------------------------------
 // Desconto de 10% aplicado nas camisetas
 
-const camisetasComDescontoAplicado = indiceCamisetas.map((valor) => precos[valor] * descontoTodasCamisetas)
-
+const descontoTodasCamisetasAplicado = indiceCamisetas.map((valor) => precos[valor] * descontoTodasCamisetas)
 // ---------------------------------------------------------------------------------------------------------------------
 // Desconto de 5% para mais de uma camiseta
-
-const descontosAdicional = 0.95;
 
 const calculaTotal = (valor1, valor2) => valor1 + valor2;
 
 const produtosParaDesconto = [];
 
-const totalSemDesconto = camisetasComDescontoAplicado
+const descontoMaisDeUmaPecaAplicado = descontoTodasCamisetasAplicado
     .map((valorProduto, indiceAtual) => {
-        if (!produtosParaDesconto.includes(valorProduto) && camisetasComDescontoAplicado.includes(valorProduto, indiceAtual + 1)) {
+        if (!produtosParaDesconto.includes(valorProduto) && descontoTodasCamisetasAplicado.includes(valorProduto, indiceAtual + 1)) {
             produtosParaDesconto.push(valorProduto);
             return valorProduto;
         }
         return valorProduto;
 
-    }).reduce(calculaTotal,0);
-
+    }).reduce(calculaTotal, 0);
 const totalDosProdutosComDesconto = produtosParaDesconto
     .reduce((acc, valorAtual) => calculaTotal(acc, valorAtual * descontosAdicional), 0);
 
 const totalASerSubtraindoDoTotal = produtosParaDesconto.reduce(calculaTotal);
 
-const totalDeCamisetaComDescontos = (totalSemDesconto - totalASerSubtraindoDoTotal + totalDosProdutosComDesconto);
+const totalDeCamisetaComDescontos = (descontoMaisDeUmaPecaAplicado - totalASerSubtraindoDoTotal + totalDosProdutosComDesconto);
 // ---------------------------------------------------------------------------------------------------------------------
 // Valor Calças Jeans com taxa
 const calcasComTaxa = indiceCalcaJeans.map(valor => precos[valor]).reduce((acc, valor) => acc += valor);
-
 // ---------------------------------------------------------------------------------------------------------------------
 // Valor Bermudas
 const valorTotalBermudas = indiceBermuda.map(valor => precos[valor])
--- public/js/total-carrinho-taxado.js
+++ public/js/total-carrinho-taxado.js
@@ -1,19 +1,11 @@
 const carrinho = [39.90, 75.99, 24, 12, 109.99, 83.99];
 
+const valorLimiteTaxa = 70.00;
+const calcularPorcentagem = valor => valor * 7 / 100;
 
-const valorLimiteTaxa = 75.00;
-const porcentagem = 7;
-let total = 0;
-
-for (let i = 0; i < carrinho.length; i++) {
-    if (carrinho[i] >= valorLimiteTaxa) {
-        let taxa = 0;
-        taxa += carrinho[i] * porcentagem / 100;
-        total += carrinho[i] + taxa;
-    }else{
-        total += carrinho[i];
-    }  
-}
-
-
+const total  = carrinho.map((valor) => valor >= valorLimiteTaxa ? calcularPorcentagem(valor) + valor: valor)
+    .reduce((acc, valor) => acc += valor)
+    
 console.log(total);
+
+
