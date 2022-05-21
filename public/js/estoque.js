c1c454185e9fb712875db6851d07e63f314f9eb4
Isaque Terra <terraisaque@gmail.com>
2022-05-21 12:17:37 -0300

    card 1 - Criando objetos literais

---
+++ public/js/estoque.js
@@ -0,0 +1,84 @@
+const estoque = [{
+    nome: 'Blusa Plus Size Feminina Ampla Decote Malha Onça Preto Filipa by Reviva Fashion',
+    url: 'blusa-plus-onca-preto',
+    preco: 139.90,
+    descricao: 'Ideal para compor looks com mood básico e estiloso, a Blusa Plus Size Feminina Ampla Decote Redondo Malha Onça Preto Filipa by Riachue é a escolha perfeita para você! Confeccionada em malha mista com linho, a blusa pode ser combinada com calça jogger e tênis casual, criando um look despojado!',
+    tamanhos_disponiveis: ['P', 'M', 'G'],
+    quantidade_disponivel: 2,
+    imagens: [
+        { url: 'assets/img/produto-1/img1.jpg', descricao: 'Modelo com Blusa Preta, Plus Size, Gola Redonda, Manga 3/4, Pequenas Estampas de Onças na Cor Bege' }
+    ],
+}, {
+    nome: 'Blusa Plus Size Feminina Ampla Folhagem Preto Filipa by Reviva Fashion',
+    url: 'blusa-plus-folhagem-preto',
+    preco: 89.90,
+    descricao: 'Ideal para compor looks versáteis e confortáveis, a Blusa Plus Size Feminina Ampla Folhagem Preto Filipa by Riachuelo é a escolha perfeita para você! Confeccionada em malha de viscose com elastano, a blusa pode ser combinada com saia midi e sandália, criando um visual elegante!',
+    tamanhos_disponiveis: ['P', 'M', 'G'],
+    quantidade_disponivel: 5,
+    imagens: [
+        { url: 'assets/img/produto-2/img1.jpg', descricao: 'Modelo com Blusa Preta, Plus Size, Gola Canoa, Manga Curta, Estampa de Folhas na Cor Verde na Barra' }
+    ],
+}, {
+    nome: 'Blusa Plus Size Feminina Gola Alta Manga Longa Canelada Verde Filipa by Reviva Fashion',
+    url: 'blusa-plus-canelada-preto',
+    preco: 79.90,
+    descricao: 'A Blusa Plus Size Feminina Gola Alta Manga Longa Canelada Verde Filipa by Riachuelo é confeccionado em malha de toque macio e é perfeito para ter sempre à mão nos dias mais frios. O modelo fechado e com modelagem regular, é básico e versátil na hora de compor um look mais quentinho, mas sem deixar o mood charmoso. Aposte!',
+    tamanhos_disponiveis: ['P', 'M', 'G'],
+    quantidade_disponivel: 7,
+    imagens: [
+        { url: 'assets/img/produto-3/img1.jpg', descricao: 'Modelo com Blusa Verde, Plus Size, Gola Alta Redonda, Manga Longa, Sem Estampas' }
+    ],
+}, {
+    nome: 'Blusa Plus Size Feminina Manga 7-8 Tricot Canelado Vermelho Filipa by Reviva Fashion',
+    url: 'blusa-plus-tricot-vermelho',
+    preco: 89.90,
+    descricao: 'Aposte na Blusa Plus Size Feminina Manga 7/8 Malha Tricot Preto Filipa by Riachuelo, a escolha perfeita para criar looks versáteis e quentinhos com muito estilo! Confeccionado em tricot, apresenta modelagem ampla e mangas 7/8, puro charme! Perfeito para looks descolados e elegantes! Combine com jeans skinny e coturno para um visual moderno!',
+    tamanhos_disponiveis: ['P', 'M', 'G'],
+    quantidade_disponivel: 9,
+    imagens: [
+        { url: 'assets/img/produto-4/img1.jpg', descricao: 'Modelo com Blusa na Cor Vermelho Escuro Mescla, Plus Size, Gola Redonda, Manga 7/8, Sem Estampas' }
+    ],
+}, {
+    nome: 'Calça Jeans Plus Size Feminina Cigarrete Denim Médio Filipa by Reviva Fashion',
+    url: 'calca-jeans-plus-cigarrete',
+    preco: 159.90,
+    descricao: 'Diferente da skinny, que também é justa, mas o comprimento da barra é um pouco mais longo, a cigarrete termina no tornozelo. Sinônimo de modernidade e estilo, a Calça Jeans Plus Size Feminina Cigarrete Denim Médio é um toque de versatilidade ao look.',
+    tamanhos_disponiveis: ['P', 'M', 'G'],
+    quantidade_disponivel: 12,
+    imagens: [
+        { url: 'assets/img/produto-5/img1.jpg', descricao: 'Modelo com Calça Jeans Cor Denim Médio, Plus Size, 2 Bolsos Frontais e 2 Traseiros, Comprimento Cigarrete, com Lavagem' }
+    ],
+}, {
+    nome: 'Top Cropped Jeans Feminino Denim Escuro Pool by Reviva Fashion',
+    url: 'top-cropped-jeans-Pool',
+    preco: 99.90,
+    descricao: 'Com toque macio, este Top Cropped Jeans Feminino Denim Escuro Pool by Reviva Fashion traz a modelagem cropped e a praticidade do jeans. Em modelo com alcinhas ajustáveis, combine com vários bottoms dando um toque de casualidade ao seu look.',
+    tamanhos_disponiveis: ['P', 'M', 'G'],
+    quantidade_disponivel: 15,
+    imagens: [
+        { url: 'assets/img/produto-6/img1.jpg', descricao: 'Modelo com Top Cropped Jeans, com Recorte Alça Média, Decote Princesa' }
+    ],
+}, {
+    nome: 'Top Cropped Plus Size Feminino Alças Franzidas Abstrato Marrom Filipa by Reviva Fashion',
+    url: 'top-cropped-alcas-franzidas',
+    preco: 129.90,
+    descricao: 'Ideal para compor looks com mood romântico e estiloso, o Top Cropped Plus Size Feminino Alças Franzidas Abstrato Marrom Filipa by Riachuelo é a escolha perfeita para você! Confeccionado em viscose, o top pode ser combinado com calça jogger de cintura alta e sandália, criando um visual incrível!',
+    tamanhos_disponiveis: ['P', 'M', 'G'],
+    quantidade_disponivel: 11,
+    imagens: [
+        { url: 'assets/img/produto-7/img1.jpg', descricao: 'Modelo com Top Cropped, Plus Size, com Recorte Alça Média, Decote Princesa, Estampa Abstrata Marrom' }
+    ],
+}, {
+    nome: 'Camisa Plus Size Feminina Manga Longa Abstrata Marrom Filipa by Reviva Fashion',
+    url: 'camisa-plus-size-marrom',
+    preco: 179.90,
+    descricao: 'Clássica e atemporal, a Camisa Plus Size Feminina Manga Longa Abstrata Marrom Filipa by Riachuelo é a escolha ideal para looks estilosos e fashionistas! Confeccionada em tecido de algodão, a peça apresenta modelagem ampla e modelo despojado. Experimente combinar com calça skinny e scarpin para um visual elegante!',
+    tamanhos_disponiveis: ['P', 'M', 'G'],
+    quantidade_disponivel: 10,
+    imagens: [
+        { url: 'assets/img/produto-8/img1.jpg', descricao: 'Modelo com Camisa, Manga Longa, Estampa Abstrata Marrom' }
+    ],
+}];
+
+
+
