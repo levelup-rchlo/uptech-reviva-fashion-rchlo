8a15825889a0c2a4e68a8490fa427c369f50578e
brMonteiro-G <brgabriel.monteiro@gmail.com>
2022-05-22 16:23:33 -0300

    feat(card-1):setting inventory

---
+++ public/js/inventory.js
@@ -0,0 +1,155 @@
+const inventory = [
+    //details.html 
+    {
+        nome: 'Blusa feminina Kaienne',
+        url: '#',
+        preco: 129.90,
+        descricao: 'A Blusa feminina Kaienne Curta Ampla Decote Redondo Folhagem Azul AK by Riachuelo é a escolha perfeita para criar looks confortáveis e práticos! Confeccionada em malha, a blusa combina super bem com calça reta e bota de cano baixo, aposte!',
+        tamanhos_disponiveis: ['P', 'M', 'G'],
+        quantidade_disponivel: 12,
+        imagens: [
+            { url: "../images/", descricao: 'Blusa feminina Kaienne no tamanho M' }
+        ],
+    },
+    {
+        nome: 'Camiseta Masculina floral',
+        url: '#',
+        preco: 69.90,
+        descricao: 'A Camiseta Masculina floral é linda para ser usada em qualquer lugar',
+        tamanhos_disponiveis: ['P', 'M', 'G'],
+        quantidade_disponivel: 12,
+        imagens: [
+            { url: '../images/', descricao: 'camiseta lipp no tamanho p' }
+        ],
+    },
+    {
+        nome: 'Camiseta Masculina floral',
+        url: '#',
+        preco: 55.90,
+        descricao: 'A Camiseta Lipp é linda para ser usada em qualquer lugar',
+        tamanhos_disponiveis: ['P', 'M', 'G'],
+        quantidade_disponivel: 12,
+        imagens: [
+            { url: '../images/', descricao: 'camiseta lipp no tamanho p' }
+        ],
+    },
+    {
+        nome: 'Camiseta Masculina floral',
+        url: '#',
+        preco: 55.90,
+        descricao: 'A Camiseta Lipp é linda para ser usada em qualquer lugar',
+        tamanhos_disponiveis: ['P', 'M', 'G'],
+        quantidade_disponivel: 12,
+        imagens: [
+            { url: '../images/', descricao: 'camiseta lipp no tamanho p' }
+        ],
+    },
+    {
+        nome: 'Camiseta Masculina floral',
+        url: '#',
+        preco: 55.90,
+        descricao: 'A Camiseta Lipp é linda para ser usada em qualquer lugar',
+        tamanhos_disponiveis: ['P', 'M', 'G'],
+        quantidade_disponivel: 12,
+        imagens: [
+            { url: '../images/', descricao: 'camiseta lipp no tamanho p' }
+        ],
+    },
+    //details.html 
+    //main.html
+    {
+        nome: 'Camiseta Masculina floral',
+        url: '#',
+        preco: 55.90,
+        descricao: 'A Camiseta Lipp é linda para ser usada em qualquer lugar',
+        tamanhos_disponiveis: ['P', 'M', 'G'],
+        quantidade_disponivel: 12,
+        imagens: [
+            { url: '../images/', descricao: 'camiseta lipp no tamanho p' }
+        ],
+    },
+    {
+        nome: 'Camiseta Masculina floral',
+        url: '#',
+        preco: 55.90,
+        descricao: 'A Camiseta Lipp é linda para ser usada em qualquer lugar',
+        tamanhos_disponiveis: ['P', 'M', 'G'],
+        quantidade_disponivel: 12,
+        imagens: [
+            { url: '../images/', descricao: 'camiseta lipp no tamanho p' }
+        ],
+    },
+    {
+        nome: 'Camiseta Masculina floral',
+        url: '#',
+        preco: 55.90,
+        descricao: 'A Camiseta Lipp é linda para ser usada em qualquer lugar',
+        tamanhos_disponiveis: ['P', 'M', 'G'],
+        quantidade_disponivel: 12,
+        imagens: [
+            { url: '../images/', descricao: 'camiseta lipp no tamanho p' }
+        ],
+    },
+    {
+        nome: 'Camiseta Masculina floral',
+        url: '#',
+        preco: 55.90,
+        descricao: 'A Camiseta Lipp é linda para ser usada em qualquer lugar',
+        tamanhos_disponiveis: ['P', 'M', 'G'],
+        quantidade_disponivel: 12,
+        imagens: [
+            { url: '../images/', descricao: 'camiseta lipp no tamanho p' }
+        ],
+    },
+    {
+        nome: 'Camiseta Masculina floral',
+        url: '#',
+        preco: 55.90,
+        descricao: 'A Camiseta Lipp é linda para ser usada em qualquer lugar',
+        tamanhos_disponiveis: ['P', 'M', 'G'],
+        quantidade_disponivel: 12,
+        imagens: [
+            { url: '../images/', descricao: 'camiseta lipp no tamanho p' }
+        ],
+    },
+    {
+        nome: 'Camiseta Masculina floral',
+        url: '#',
+        preco: 55.90,
+        descricao: 'A Camiseta Lipp é linda para ser usada em qualquer lugar',
+        tamanhos_disponiveis: ['P', 'M', 'G'],
+        quantidade_disponivel: 12,
+        imagens: [
+            { url: '../images/', descricao: 'camiseta lipp no tamanho p' }
+        ],
+    },
+    {
+        nome: 'Camiseta Masculina floral',
+        url: '#',
+        preco: 55.90,
+        descricao: 'A Camiseta Lipp é linda para ser usada em qualquer lugar',
+        tamanhos_disponiveis: ['P', 'M', 'G'],
+        quantidade_disponivel: 12,
+        imagens: [
+            { url: '../images/', descricao: 'camiseta lipp no tamanho p' }
+        ],
+    },
+    {
+        nome: 'Camiseta Masculina floral',
+        url: '#',
+        preco: 55.90,
+        descricao: 'A Camiseta Lipp é linda para ser usada em qualquer lugar',
+        tamanhos_disponiveis: ['P', 'M', 'G'],
+        quantidade_disponivel: 12,
+        imagens: [
+            { url: '../images/', descricao: 'camiseta lipp no tamanho p' }
+        ],
+    }
+
+    //main.html
+
+]
+
+localStorage.setItem("itens", JSON.stringify(inventory))
+
+
