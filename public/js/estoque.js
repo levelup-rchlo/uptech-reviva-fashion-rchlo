e691b574a46c7e9fc21c140312e3bc69dd06b40e
Osmarv <osmarv@gmail.com>
2022-05-23 11:27:50 -0300

    Criando objetos literais & update products on index

---
+++ public/js/estoque.js
@@ -0,0 +1,48 @@
+const estoque = [{
+        nome: 'Cropped preto AK by Riachuelo',
+        url: 'cropped-preto-ak-by-riachuelo',
+        preco: 29.90,
+        descricao: 'O Cropped preto AK by Riachuelo tem o charme do preto e básico. Ótimo para looks de verão!',
+        tamanhos_disponiveis: ['P', 'M', 'G'],
+        quantidade_disponivel: 3,
+        imagens: [
+            { url: 'link para imagem', descricao: 'crooped preto' }
+        ],
+    },
+
+    {
+        nome: 'Regata listrada AK by Riachuelo',
+        url: 'regata-listrada-ak-by-riachuelo',
+        preco: 39.90,
+        descricao: 'Confeccionada em mix de fibras, a Regata listrada AK by Riachuelo vai fazer você se apaixonar! Com design único, a peça apresenta caimento perfeito e tecido super confortável, perfeita para diversas ocasiões, aposte!',
+        tamanhos_disponiveis: ['P', 'M', 'G'],
+        quantidade_disponivel: 3,
+        imagens: [
+            { url: 'link para imagem', descricao: 'Regata listrada preta e cinza' }
+        ],
+    },
+
+    {
+        nome: 'Calça larga preto AK by Riachuelo',
+        url: 'calça-larga-preto-ak-by-riachuelo',
+        preco: 99.90,
+        descricao: 'A Calça larga preto AK by Riachuelo é a escolha certa para criar looks com muito estilo! Confeccionada em sarja, a calça apresenta modelagem paper bag, a queridinha do momento! Perfeita para ocasiões especiais, encontros com os amigos e com o crush, aposte!',
+        tamanhos_disponiveis: ['P', 'M', 'G'],
+        quantidade_disponivel: 4,
+        imagens: [
+            { url: 'link para imagem', descricao: 'Calça larga preta' }
+        ],
+    },
+
+    {
+        nome: 'Vestido amarelo AK by Riachuelo',
+        url: 'calca-alfaiataria',
+        preco: 179.90,
+        descricao: 'Blazer Cropped Feminino Manga Longa com Ombreira Linho Bege AK by Riachuelo confeccionado em tecido com linho, a peça conta com modelagem curta e gola tailleur com fechamento por botão. Queridinho do momento, o blazer pode ser usado em várias composições, como em peças com jeans destroyed e tênis casual ou com calça de alfaiataria e salto fino, formando um look super elegante. Inspire-se!',
+        tamanhos_disponiveis: ['P', 'M', 'G'],
+        quantidade_disponivel: 4,
+        imagens: [
+            { url: 'link para imagem', descricao: 'Vestido amarelo' }
+        ],
+    }
+]
