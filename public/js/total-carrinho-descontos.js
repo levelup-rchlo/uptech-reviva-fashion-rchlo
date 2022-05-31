8aa64d35888eb5ff2e960db7313a417dbe420307
Osmarv <osmarv@gmail.com>
2022-05-31 14:50:55 -0300

    Convertendo o código

--- public/js/total-carrinho-descontos.js
+++ public/js/total-carrinho-descontos.js
@@ -1,36 +1,28 @@
-const carrinho = [25.99, 27.83, 35.80, 35.80, 35.80, 44, 53.90, 25.99]
-
+"use strict";
+const carrinhoTaxadoDesconto = [25.99, 27.83, 35.80, 35.80, 35.80, 44, 53.90, 25.99];
 //console.log(25.99 + 27.83 + 35.80 + 35.80 + 35.80 + 44 + 53.90 + 25.99)
 //console.log(25.99 + 27.83 + 35.80 + (35.80 - (0.1 * 38.80)) + 35.80 + 44 + 53.90 + (25.99 - (0.1 * 25.99)))
-
-const carrinhoRepetidos = []
-const carrinhoExtras = []
-
-var semRepetidos = carrinho.filter(function(current, index) {
-    if (!(carrinho.indexOf(current) === index)) {
+const carrinhoRepetidos = [];
+const carrinhoExtras = [];
+var semRepetidos = carrinhoTaxadoDesconto.filter(function (current, index) {
+    if (!(carrinhoTaxadoDesconto.indexOf(current) === index)) {
         if (carrinhoRepetidos.indexOf(current) === -1) {
-            carrinhoRepetidos.push(current)
-        } else {
-            carrinhoExtras.push(current)
+            carrinhoRepetidos.push(current);
+        }
+        else {
+            carrinhoExtras.push(current);
         }
     }
-    return carrinho.indexOf(current) === index;
+    return carrinhoTaxadoDesconto.indexOf(current) === index;
 });
-
-
 for (let i = 0; i < carrinhoRepetidos.length; i++) {
-    carrinhoRepetidos[i] -= 0.1 * carrinhoRepetidos[i]
+    carrinhoRepetidos[i] -= 0.1 * carrinhoRepetidos[i];
 }
-
-const arrayPrincipal = semRepetidos.concat(carrinhoExtras, carrinhoRepetidos)
-
-total = 0
-
+const arrayPrincipal = semRepetidos.concat(carrinhoExtras, carrinhoRepetidos);
+let totalTaxadoDescontos = 0;
 arrayPrincipal.forEach(value => {
-    total += value
-})
-
-console.log(total)
-
+    totalTaxadoDescontos += value;
+});
+console.log(totalTaxadoDescontos);
 //console.log(arrayPrincipal);
 //console.log(carrinhoRepetidos);
