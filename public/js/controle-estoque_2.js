c9794860123f4ee4874b4c9cf0bd43b33d537e82
Geldo Pina <geldo.costa@riachuelo.com.br>
2022-06-20 15:42:32 -0300

    correção desafio 2

--- public/js/controle-estoque_2.js
+++ public/js/controle-estoque_2.js
@@ -13,8 +13,32 @@ const addStock = (stockList, product) => {
   return stockList;
 };
 
+const removeStock = (stockList, product) => {
+  let error = "";
+  let productFound = stockList.find((o) => o[0] === product[0]);
+  if (!productFound) {
+    error = "Produto não encontrado!";
+    console.error(error);
+    // throw new Error(error);
+  } else {
+    product[1] = product[1] ? product[1] : 1;
+    if (product[1] <= productFound[1]) {
+      productFound[1] -= product[1] ? product[1] : 1;
+    } else {
+      error = "Quantidade informada não está disponível!";
+      console.error(error);
+      // throw new Error(error);
+    }
+  }
+  return stockList;
+};
+
 console.log("> ADD STOCK:", addStock(stock, ["Camiseta Masculina", 2, 29.9]));
 console.log("> ADD STOCK:", addStock(stock, ["Camiseta Flanela", 10, 29.9]));
 console.log("> ADD STOCK:", addStock(stock, ["Calça Feminina", 1, 29.9]));
 console.log("> ADD STOCK:", addStock(stock, ["Calça Jeans", 1, 29.9]));
 console.log("> ADD STOCK:", addStock(stock, ["Camiseta Masculina", 20, 29.9]));
+console.log("> DEL STOCK:", removeStock(stock, ["Camiseta Masculina"]));
+console.log("> DEL STOCK:", removeStock(stock, ["Camiseta Masculina", 20]));
+console.log("> DEL STOCK:", removeStock(stock, ["Camiseta Nike", 20]));
+console.log("> DEL STOCK:", removeStock(stock, ["Calça Jeans", 2, 29.9]));
