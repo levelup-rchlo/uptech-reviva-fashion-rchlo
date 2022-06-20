001c6a420ce041180541b1578ddfdf9da1431d2b
Geldo Pina <geldo.costa@riachuelo.com.br>
2022-06-20 14:34:00 -0300

    refatoração desafio estoque 1

--- public/js/controle-estoque_2.js
+++ public/js/controle-estoque_2.js
@@ -1,22 +1,20 @@
-const estoque = [
+const stock = [
   ["Camiseta Masculina", 5, 29.9],
   ["Calça Feminina", 8, 108.9],
 ];
 
-const addStock = (estoque, produto) => {
-  let p = estoque.find((o) => o[0] === produto[0]);
-  if (p) {
-    p[1] += produto[1];
+const addStock = (stockList, product) => {
+  let productFound = stockList.find((o) => o[0] === product[0]);
+  if (productFound) {
+    productFound[1] += product[1];
   } else {
-    estoque.push(produto);
+    stockList.push(product);
   }
-  return estoque;
+  return stockList;
 };
-console.log("> ADD STOCK:", addStock(estoque, ["Camiseta Masculina", 2, 29.9]));
-console.log("> ADD STOCK:", addStock(estoque, ["Camiseta Flanela", 10, 29.9]));
-console.log("> ADD STOCK:", addStock(estoque, ["Calça Feminina", 1, 29.9]));
-console.log("> ADD STOCK:", addStock(estoque, ["Calça Jeans", 1, 29.9]));
-console.log(
-  "> ADD STOCK:",
-  addStock(estoque, ["Camiseta Masculina", 20, 29.9])
-);
+
+console.log("> ADD STOCK:", addStock(stock, ["Camiseta Masculina", 2, 29.9]));
+console.log("> ADD STOCK:", addStock(stock, ["Camiseta Flanela", 10, 29.9]));
+console.log("> ADD STOCK:", addStock(stock, ["Calça Feminina", 1, 29.9]));
+console.log("> ADD STOCK:", addStock(stock, ["Calça Jeans", 1, 29.9]));
+console.log("> ADD STOCK:", addStock(stock, ["Camiseta Masculina", 20, 29.9]));
